9f1ec6018e32aaed4b02f1f24abf2fe88b419f2b
DeokJin <33983084+qute012@users.noreply.github.com>
2021-10-01 09:46:27 +0900

    Create loss.py

---
+++ loss.py
@@ -0,0 +1,23 @@
+class ContrastiveWeightedMarginLoss(nn.Module):
+    def __init__(self, reduce='none'):
+        super(ContrastiveWeightedMarginLoss, self).__init__()
+        self.reduce = reduce
+        self.sim = nn.CosineSimilarity(dim=-1, eps=1e-6)
+
+    def forward(self, contexts, positives, negatives, margin=0.5):
+        pos_sim = self.sim(contexts, positives)
+        neg_sim = self.sim(contexts, negatives)
+        logits = pos_sim - neg_sim + torch.log(margin + 1)
+        logits = logits.view(-1, logits.size(-1))
+
+        target = logits.new_zeros(contexts.size(0)*contexts.size(1), dtype=torch.float)
+
+        denom = contexts.size(0)
+        loss = max(logits, target)
+
+        if self.reduce == 'none':
+             return loss
+        elif self.reduce == 'sum':
+            return loss.sum()
+        else:
+            return loss.sum()/denom
